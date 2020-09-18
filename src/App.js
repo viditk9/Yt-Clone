8bd68d678bf155488757d19757678dbc936b22f4
viditk9 <viditkhodiar@gmail.com>
2020-09-18 20:52:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Grid} from '@material-ui/core';
+
+import {SearchBar, VideoDetail, VideoList} from './components';
+
+import youtube from './api/youtube';
+
+import Header from "./Header.jsx";
+
+import './styles.css';
+
+class App extends React.Component {
+
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    componentDidMount(){
+        this.handleSubmit('Nature');
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+            part: 'snippet',
+            maxResults: 5,
+            key: 'AIzaSyCkUHN60eZPtaOmiX7_qru2n5R6b476nMg',
+            q: searchTerm,
+            }
+        });
+        console.log(response.data.items);
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0] });
+    }
+
+    render (){
+        const {selectedVideo, videos} = this.state;
+        return(
+            <div>
+            <Header/>
+            <Grid  justify="center" container spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar  onFormSubmit = {this.handleSubmit} />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail video={selectedVideo} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <VideoList videos={videos} onVideoSelect = {this.onVideoSelect} />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        Utube
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
